e804719ffd589aaba3813c6465f45970e89b83f9
Ernest W. Durbin III <ewdurbin@gmail.com>
2018-01-17 17:32:37 -0500

    tidy up some templates, sort views a bit more logically

--- cabotage/server/user/views.py
+++ cabotage/server/user/views.py
@@ -22,19 +22,73 @@ from cabotage.server.user.forms import (
 user_blueprint = Blueprint('user', __name__,)
 
 
+@user_blueprint.route('/organizations')
+@login_required
+def organizations():
+    user = current_user
+    organizations = user.organizations
+    return render_template('user/organizations.html', organizations=organizations)
+
+
+@user_blueprint.route('/organizations/<org_slug>')
+@login_required
+def organization(org_slug):
+    user = current_user
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    return render_template('user/organization.html', organization=organization)
+
+
+@user_blueprint.route('/organizations/create', methods=["GET", "POST"])
+@login_required
+def organization_create():
+    user = current_user
+    form = CreateOrganizationForm()
+
+    if form.validate_on_submit():
+        organization = Organization(name=form.name.data, slug=form.slug.data)
+        organization.add_user(user, admin=True)
+        db.session.add(organization)
+        db.session.commit()
+        return redirect(url_for('user.organization', org_slug=organization.slug))
+    return render_template('user/organization_create.html', organization_create_form=form)
+
+
+@user_blueprint.route('/organizations/<org_slug>/projects')
+@login_required
+def organization_projects(org_slug):
+    user = current_user
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    return render_template('user/organization_projects.html', organization=organization)
+
+
+@user_blueprint.route('/organizations/<org_slug>/projects/create', methods=["GET", "POST"])
+@login_required
+def organization_project_create(org_slug):
+    user = current_user
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+
+    form = CreateProjectForm()
+    form.organization_id.choices = [(str(organization.id), organization.name)]
+    form.organization_id.data = str(organization.id)
+
+    if form.validate_on_submit():
+        project = Project(organization_id=organization.id, name=form.name.data, slug=form.slug.data)
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('user.project', org_slug=project.organization.slug, project_slug=project.slug))
+    return render_template('user/organization_project_create.html', organization=organization, organization_project_create_form=form)
+
+
 @user_blueprint.route('/projects')
 @login_required
 def projects():
     return render_template('user/projects.html', projects=current_user.projects)
-
-
-@user_blueprint.route('/projects/<org_slug>')
-@login_required
-def org_projects(org_slug):
-    organizations = Organization.query.filter_by(slug=org_slug).all()
-    if organizations == []:
-        abort(404)
-    return render_template('user/org_projects.html', organizations=organizations)
 
 
 @user_blueprint.route('/projects/<org_slug>/<project_slug>')
@@ -65,6 +119,63 @@ def project_application(org_slug, project_slug, app_slug):
         'user/project_application.html',
         application=application,
     )
+
+
+@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/create', methods=["GET", "POST"])
+@login_required
+def project_application_create(org_slug, project_slug):
+    user = current_user
+    form = CreateApplicationForm()
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
+    if project is None:
+        abort(404)
+
+    form.organization_id.choices = [(str(organization.id), organization.name)]
+    form.project_id.choices = [(str(project.id), project.name)]
+    form.organization_id.data = str(organization.id)
+    form.project_id.data = str(project.id)
+
+    if form.validate_on_submit():
+        application = Application(project_id=form.project_id.data, name=form.name.data, slug=form.slug.data)
+        db.session.add(application)
+        db.session.commit()
+        return redirect(url_for('user.project_application', org_slug=project.organization.slug, project_slug=project.slug, app_slug=application.slug))
+    return render_template('user/project_application_create.html', project_application_create_form=form, org_slug=org_slug, project_slug=project_slug)
+
+
+@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications')
+@login_required
+def project_applications(org_slug, project_slug):
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
+    if project is None:
+        abort(404)
+    return render_template('user/project_applications.html', project=project)
+
+
+@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/config/<config_id>')
+@login_required
+def project_application_configuration(org_slug, project_slug, app_slug, config_id):
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
+    if project is None:
+        abort(404)
+    application = Application.query.filter_by(project_id=project.id, slug=app_slug).first()
+    if application is None:
+        abort(404)
+    configuration = Configuration.query.filter_by(application_id=application.id, id=config_id).first()
+    if configuration is None:
+        abort(404)
+
+    return render_template('user/project_application_configuration.html', configuration=configuration)
+
 
 @user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/config/create', methods=['GET', 'POST'])
 @login_required
@@ -170,6 +281,25 @@ def project_application_configuration_delete(org_slug, project_slug, app_slug, c
     return render_template('user/project_application_configuration_delete.html', form=form, org_slug=organization.slug, project_slug=project.slug, app_slug=application.slug, configuration=configuration)
 
 
+@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/container/<container_id>')
+@login_required
+def project_application_container(org_slug, project_slug, app_slug, container_id):
+    organization = Organization.query.filter_by(slug=org_slug).first()
+    if organization is None:
+        abort(404)
+    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
+    if project is None:
+        abort(404)
+    application = Application.query.filter_by(project_id=project.id, slug=app_slug).first()
+    if application is None:
+        abort(404)
+    container = Container.query.filter_by(application_id=application.id, id=container_id).first()
+    if container is None:
+        abort(404)
+
+    return render_template('user/project_application_container.html', container=container)
+
+
 @user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/container/create', methods=['GET', 'POST'])
 @login_required
 def project_application_container_create(org_slug, project_slug, app_slug):
@@ -199,25 +329,6 @@ def project_application_container_create(org_slug, project_slug, app_slug):
     return render_template('user/project_application_container_create.html', form=form, org_slug=organization.slug, project_slug=project.slug, app_slug=application.slug)
 
 
-@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/container/<container_id>')
-@login_required
-def project_application_container(org_slug, project_slug, app_slug, container_id):
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
-    if project is None:
-        abort(404)
-    application = Application.query.filter_by(project_id=project.id, slug=app_slug).first()
-    if application is None:
-        abort(404)
-    container = Container.query.filter_by(application_id=application.id, id=container_id).first()
-    if container is None:
-        abort(404)
-
-    return render_template('user/project_application_container.html', container=container)
-
-
 @user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/container/<container_id>/edit', methods=['GET', 'POST'])
 @login_required
 def project_application_container_edit(org_slug, project_slug, app_slug, container_id):
@@ -243,137 +354,3 @@ def project_application_container_edit(org_slug, project_slug, app_slug, contain
         db.session.commit()
         return redirect(url_for('user.project_application', org_slug=organization.slug, project_slug=project.slug, app_slug=application.slug))
     return render_template('user/project_application_container_edit.html', form=form, org_slug=organization.slug, project_slug=project.slug, app_slug=application.slug, container=container)
-
-
-@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications')
-@login_required
-def project_applications(org_slug, project_slug):
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
-    if project is None:
-        abort(404)
-    return render_template('user/project_applications.html', project=project)
-
-
-@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/<app_slug>/config/<config_id>')
-@login_required
-def project_application_configuration(org_slug, project_slug, app_slug, config_id):
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
-    if project is None:
-        abort(404)
-    application = Application.query.filter_by(project_id=project.id, slug=app_slug).first()
-    if application is None:
-        abort(404)
-    configuration = Configuration.query.filter_by(application_id=application.id, id=config_id).first()
-    if configuration is None:
-        abort(404)
-
-    return render_template('user/project_application_configuration.html', configuration=configuration)
-
-
-@user_blueprint.route('/projects/<org_slug>/<project_slug>/applications/create', methods=["GET", "POST"])
-@login_required
-def project_application_create(org_slug, project_slug):
-    user = current_user
-    form = CreateApplicationForm()
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    project = Project.query.filter_by(organization_id=organization.id, slug=project_slug).first()
-    if project is None:
-        abort(404)
-
-    form.organization_id.choices = [(str(organization.id), organization.name)]
-    form.project_id.choices = [(str(project.id), project.name)]
-    form.organization_id.data = str(organization.id)
-    form.project_id.data = str(project.id)
-
-    if form.validate_on_submit():
-        application = Application(project_id=form.project_id.data, name=form.name.data, slug=form.slug.data)
-        db.session.add(application)
-        db.session.commit()
-        return redirect(url_for('user.project_application', org_slug=project.organization.slug, project_slug=project.slug, app_slug=application.slug))
-    return render_template('user/project_application_create.html', project_application_create_form=form, org_slug=org_slug, project_slug=project_slug)
-
-
-@user_blueprint.route('/projects/create', methods=["GET", "POST"])
-@login_required
-def project_create():
-    user = current_user
-    form = CreateProjectForm()
-    form.organization_id.choices = [(str(o.organization_id), o.organization.name) for o in user.organizations]
-
-    if form.validate_on_submit():
-        project = Project(organization_id=form.organization_id.data, name=form.name.data, slug=form.slug.data)
-        db.session.add(project)
-        db.session.commit()
-        return redirect(url_for('user.project', org_slug=project.organization.slug, project_slug=project.slug))
-    return render_template('user/project_create.html', project_create_form=form)
-
-
-@user_blueprint.route('/organizations/<org_slug>/projects/create', methods=["GET", "POST"])
-@login_required
-def organization_project_create(org_slug):
-    user = current_user
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-
-    form = CreateProjectForm()
-    form.organization_id.choices = [(str(organization.id), organization.name)]
-    form.organization_id.data = str(organization.id)
-
-    if form.validate_on_submit():
-        project = Project(organization_id=organization.id, name=form.name.data, slug=form.slug.data)
-        db.session.add(project)
-        db.session.commit()
-        return redirect(url_for('user.project', org_slug=project.organization.slug, project_slug=project.slug))
-    return render_template('user/organization_project_create.html', organization=organization, organization_project_create_form=form)
-
-
-@user_blueprint.route('/organizations/<org_slug>')
-@login_required
-def organization(org_slug):
-    user = current_user
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    return render_template('user/organization.html', organization=organization)
-
-
-@user_blueprint.route('/organizations/<org_slug>/projects')
-@login_required
-def organization_projects(org_slug):
-    user = current_user
-    organization = Organization.query.filter_by(slug=org_slug).first()
-    if organization is None:
-        abort(404)
-    return render_template('user/organization_projects.html', organization=organization)
-
-
-@user_blueprint.route('/organizations')
-@login_required
-def organizations():
-    user = current_user
-    organizations = user.organizations
-    return render_template('user/organizations.html', organizations=organizations)
-
-
-@user_blueprint.route('/organizations/create', methods=["GET", "POST"])
-@login_required
-def organization_create():
-    user = current_user
-    form = CreateOrganizationForm()
-
-    if form.validate_on_submit():
-        organization = Organization(name=form.name.data, slug=form.slug.data)
-        organization.add_user(user, admin=True)
-        db.session.add(organization)
-        db.session.commit()
-        return redirect(url_for('user.organization', org_slug=organization.slug))
-    return render_template('user/organization_create.html', organization_create_form=form)
