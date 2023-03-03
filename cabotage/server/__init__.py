af0a637c9605df4b0e687a5b8fb229400dc63733
Ee Durbin <ewdurbin@gmail.com>
2023-03-03 06:48:28 -0500

    factor nav into its own module

--- cabotage/server/__init__.py
+++ cabotage/server/__init__.py
@@ -18,6 +18,7 @@ from celery import Task
 from sqlalchemy import MetaData
 
 from cabotage.server.acl import cabotage_on_identity_loaded
+from cabotage.server.nav import nav
 
 from cabotage.server.ext.consul import Consul
 from cabotage.server.ext.vault import Vault
@@ -84,36 +85,6 @@ def create_app():
         ExtendedLoginForm,
         ExtendedRegisterForm,
     )
-
-    from flask_nav3 import Nav
-    from flask_nav3.elements import Navbar, View, Separator, Subgroup
-
-    nav = Nav()
-
-    anonymous_nav = Navbar(
-        'Cabotage',
-        View('Register', 'security.register'),
-        View('Log In', 'security.login'),
-    )
-    logged_in_nav = Navbar(
-        'Cabotage',
-        Subgroup(
-            'Orgs',
-            View('All My Orgs', 'user.organizations'),
-        ),
-        Subgroup(
-            'Projects',
-            View('All My Projects', 'user.projects'),
-        ),
-        Subgroup(
-          'Account',
-          Separator(),
-          View('Change Password', 'security.change_password'),
-          View('Log Out', 'security.logout'),
-        ),
-    )
-    nav.register_element('anonymous', anonymous_nav)
-    nav.register_element('logged_in', logged_in_nav)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS', 'cabotage.server.config.Config')
---
+++ cabotage/server/nav.py
@@ -0,0 +1,29 @@
+from flask_nav3 import Nav
+from flask_nav3.elements import Navbar, View, Separator, Subgroup
+
+nav = Nav()
+
+anonymous_nav = Navbar(
+    'Cabotage',
+    View('Register', 'security.register'),
+    View('Log In', 'security.login'),
+)
+logged_in_nav = Navbar(
+    'Cabotage',
+    Subgroup(
+        'Orgs',
+        View('All My Orgs', 'user.organizations'),
+    ),
+    Subgroup(
+        'Projects',
+        View('All My Projects', 'user.projects'),
+    ),
+    Subgroup(
+      'Account',
+      Separator(),
+      View('Change Password', 'security.change_password'),
+      View('Log Out', 'security.logout'),
+    ),
+)
+nav.register_element('anonymous', anonymous_nav)
+nav.register_element('logged_in', logged_in_nav)
