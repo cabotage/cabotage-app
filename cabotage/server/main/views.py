0452e5a272de8631b57c21e6b144322531c75ecd
Jacob Coffee <jacob@z7x.org>
2024-09-25 16:02:23 -0500

    feat: add users orgs and projectsto home page if logged in

--- cabotage/server/main/views.py
+++ cabotage/server/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint
+from flask_security import current_user
 
 
 main_blueprint = Blueprint(
@@ -9,7 +10,12 @@ main_blueprint = Blueprint(
 
 @main_blueprint.route("/")
 def home():
-    return render_template("main/home.html")
+    if current_user.is_authenticated:
+        projects = current_user.projects
+        organizations = current_user.organizations
+    else:
+        projects, organizations = None, None
+    return render_template("main/home.html", projects=projects, organizations=organizations)
 
 
 @main_blueprint.route("/about/")
