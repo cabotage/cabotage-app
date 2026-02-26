6c143f787f9551a7e1159ca6fbcac1021d691c93
Jacob Coffee <jacob@z7x.org>
2026-02-26 15:24:19 -0600

    wrap github in try except to show errors (#160)

--- cabotage/utils/github.py
+++ cabotage/utils/github.py
@@ -1,14 +1,27 @@
+import logging
+
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 def post_deployment_status_update(access_token, status_url, state, description):
-    requests.post(
-        status_url,
-        headers={
-            "Accept": "application/vnd.github.ant-man-preview+json",
-            "Authorization": f"token {access_token}",
-            "Content-Type": "application/json",
-        },
-        json={"state": state, "description": description},
-        timeout=10,
-    )
+    if access_token is None:
+        return
+    try:
+        requests.post(
+            status_url,
+            headers={
+                "Accept": "application/vnd.github.ant-man-preview+json",
+                "Authorization": f"token {access_token}",
+                "Content-Type": "application/json",
+            },
+            json={"state": state, "description": description},
+            timeout=10,
+        )
+    except requests.exceptions.RequestException:
+        logger.exception(
+            "Failed to post deployment status update to %s (state=%s)",
+            status_url,
+            state,
+        )
