5fd2f6a03981ee928c876117ffc1ffcc35c0029c
Ee Durbin <ewdurbin@gmail.com>
2026-03-04 13:34:28 -0500

    restore vanity github deployment environment names

--- cabotage/server/models/projects.py
+++ cabotage/server/models/projects.py
@@ -352,7 +352,9 @@ class ApplicationEnvironment(db.Model, Timestamp):
     def effective_github_environment_name(self):
         if self.github_environment_name is not None:
             return self.github_environment_name
-        return f"{self.application.project.slug}/{self.environment.slug}/{self.application.slug}"
+        if self.application.github_environment_name is not None:
+            return self.application.github_environment_name
+        return f"{self.application.project.organization.slug}/{self.application.project.slug}/{self.environment.slug}/{self.application.slug}"
 
     @property
     def effective_deployment_timeout(self):
