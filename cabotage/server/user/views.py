8ea562f4ad9e5f59daadcffd17c8b4c5f6011aec
Jacob Coffee <jacob@z7x.org>
2026-03-13 20:30:05 -0500

    no extra lines pls

--- cabotage/server/user/views.py
+++ cabotage/server/user/views.py
@@ -2898,7 +2898,6 @@ def application_images_build_fromsource(org_slug, project_slug, app_slug):
     )
 
     build_ref = app_env.effective_auto_deploy_branch or "main"
-
     image = Image(
         application_id=application.id,
         application_environment_id=app_env.id,
