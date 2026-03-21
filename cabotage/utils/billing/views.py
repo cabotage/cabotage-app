77a0623721b5aebdaf3b3b114cdf46c3bd570ee4
Jacob Coffee <jacob@z7x.org>
2026-03-21 13:02:38 -0500

    initial work for ui

---
+++ cabotage/utils/billing/views.py
@@ -0,0 +1,37 @@
+"""Views for Stipre."""
+from flask import render_template
+from flask_login import login_required
+
+from cabotage.server.models import Organization
+from cabotage.utils.billing.core import stripe_blueprint
+
+
+@stripe_blueprint.route("/<org_slug>", methods=["POST"])
+@login_required
+def dashboard(org_slug: str) -> str:
+    """Render the Stripe dashboard."""
+    org = Organization.query.filter_by(slug=org_slug).first_or_404()
+    return render_template("billing/dashboard.html", org=org)
+
+@stripe_blueprint.route("/<org_slug>/invoices", methods=["GET", "POST"])
+@login_required
+def invoice(org_slug: str) -> str:
+    """Render the Stripe invoice list."""
+    ...
+
+@stripe.blueprint.route("/<org_slug>/subscribe", methods=["GET", "POST"])
+@login_required
+def subscribe(org_slug: str) -> str:
+    """Subscription UI."""
+    org = Organization.query.filter_by(slug=org_slug).first_or_404()
+    # get can show the plan picker since its ui
+    # post will handle the stripe payment elemtn, return client secret
+    return render_template("billing/subscribe.html", org=org)
+
+@stripe.blueprint.route("/<org_slug>/payment", methods=["GET", "POST"])
+@login_required
+def payment_methods(org_slug: str) -> str:
+    """Subscription UI."""
+    org = Organization.query.filter_by(slug=org_slug).first_or_404()
+    return render_template("billing/payment_methods.html", org=org)
+
