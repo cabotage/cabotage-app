1a158b7ab4d87fcc5b1e1986e90e7e5e9dc6e014
Ee Durbin <ewdurbin@gmail.com>
2026-04-01 07:52:33 -0400

    black -> ruff format

--- cabotage/celery/tasks/deploy.py
+++ cabotage/celery/tasks/deploy.py
@@ -1047,7 +1047,7 @@ def fetch_ingress(networking_api, release, ingress):
         if exc.status == 404:
             return create_ingress(networking_api, release, ingress)
         raise DeployError(
-            "Unexpected exception fetching Ingress/" f"{k8s_name} in {namespace}: {exc}"
+            f"Unexpected exception fetching Ingress/{k8s_name} in {namespace}: {exc}"
         )
 
 
@@ -1535,7 +1535,7 @@ def render_process_container(
     pod_class = pod_classes[process_pod_cls]
     return kubernetes.client.V1Container(
         name=process_name,
-        image=f'{current_app.config["REGISTRY_PULL"]}/{release.repository_name}:release-{release.version}',
+        image=f"{current_app.config['REGISTRY_PULL']}/{release.repository_name}:release-{release.version}",
         image_pull_policy="Always",
         env=[
             kubernetes.client.V1EnvVar(
@@ -1604,10 +1604,12 @@ def render_datadog_container(dd_api_key, datadog_tags):
             kubernetes.client.V1EnvVar(name="DD_APM_ENABLED", value="true"),
             kubernetes.client.V1EnvVar(name="DD_LOGS_ENABLED", value="false"),
             kubernetes.client.V1EnvVar(
-                name="DD_CONFD_PATH", value="/tmp/null"  # nosec
+                name="DD_CONFD_PATH",
+                value="/tmp/null",  # nosec
             ),
             kubernetes.client.V1EnvVar(
-                name="DD_AUTOCONF_TEMPLATE_DIR", value="/tmp/null"  # nosec
+                name="DD_AUTOCONF_TEMPLATE_DIR",
+                value="/tmp/null",  # nosec
             ),
             kubernetes.client.V1EnvVar(name="DD_ENABLE_GOHAI", value="false"),
             kubernetes.client.V1EnvVar(
